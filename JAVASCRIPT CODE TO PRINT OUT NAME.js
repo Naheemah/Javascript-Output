5c1ad44a3abbafbf6a87cfe23152bfe02e9f31e3
Naheemah <91073992+Naheemah@users.noreply.github.com>
2022-06-21 07:13:10 +0100

    Javascript output

---
+++ JAVASCRIPT CODE TO PRINT OUT NAME.js
@@ -0,0 +1,7 @@
+const name = "Naheemah Abdulazeez";
+const country = "Nigeria";
+document.write("Name: " + name);
+document.write("<br>");
+document.write("Height: " + 5 + "ft");
+document.write("<br>");
+document.write("Country: " + country);
